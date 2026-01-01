2ce680a6fdca02f37a194b1f6898f7b4fc00aeb5
imSaquib-web <saquib1230@gmail.com>
2026-01-02 01:57:56 +0530

    Backend Connection

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:6060/api",
+  baseURL: "https://real-estate-web-server-teal.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
